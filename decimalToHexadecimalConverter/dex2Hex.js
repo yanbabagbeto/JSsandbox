b123579bb006fcd8648b08ce3de9e7b65dca1e4b
Yan Babagbeto <baby3102@usherbrooke.ca>
2023-01-28 21:58:24 -0500

    modified

--- decimalToHexadecimalConverter/dex2Hex.js
+++ decimalToHexadecimalConverter/dex2Hex.js
@@ -10,77 +10,77 @@ convertButton.addEventListener("click", convert);
 resetButton.addEventListener("click", resetFields);
 
 function resetFields() {
-  array.length = 0;
-  if (mainInput.value != "") {
-    mainInput.value = "";
-  }
-  // resultField.innerText = "";
-  location.reload(true);
+	array.length = 0;
+	if (mainInput.value != "") {
+		mainInput.value = "";
+	}
+	// resultField.innerText = "";
+	location.reload(true);
 }
 
 function convert() {
-  let number = parseInt(document.getElementById("main_input").value);
-  let divider = number;
-  let quotient, remainder;
+	let number = parseInt(document.getElementById("main_input").value);
+	let divider = number;
+	let quotient, remainder;
 
-  if (divider < 16) {
-    toHex(divider);
-    quotient = Math.floor(divider / 16);
-    remainder = divider % 16;
-    console.log(
-      array,
-      `divider: ${divider}, quotient: ${quotient}, remainder: ${remainder}`
-    );
-  } else {
-    while (divider >= 16) {
-      quotient = Math.floor(divider / 16);
-      remainder = divider % 16;
-      toHex(remainder);
-      console.log(
-        array,
-        `divider: ${divider}, quotient: ${quotient}, remainder: ${remainder}`
-      );
+	if (divider < 16) {
+		toHex(divider);
+		quotient = Math.floor(divider / 16);
+		remainder = divider % 16;
+		console.log(
+			array,
+			`divider: ${divider}, quotient: ${quotient}, remainder: ${remainder}`,
+		);
+	} else {
+		while (divider >= 16) {
+			quotient = Math.floor(divider / 16);
+			remainder = divider % 16;
+			toHex(remainder);
+			console.log(
+				array,
+				`divider: ${divider}, quotient: ${quotient}, remainder: ${remainder}`,
+			);
 
-      divider = quotient;
-    }
-    array.push(divider.toString());
-    console.log(array);
-    console.log(array.reverse());
-  }
+			divider = quotient;
+		}
+		array.push(divider.toString());
+		console.log(array);
+		console.log(array.reverse());
+	}
 
-  displayResult();
+	displayResult();
 }
 
 function displayResult() {
-  let result = "";
-  for (item of array) {
-    result += item;
-  }
-  console.log(result);
-  resultField.innerText = result;
+	let result = "";
+	for (item of array) {
+		result += item;
+	}
+	console.log(result);
+	resultField.innerText = result;
 }
 
 function toHex(val) {
-  switch (val) {
-    case 10:
-      array.push("A");
-      break;
-    case 11:
-      array.push("B");
-      break;
-    case 12:
-      array.push("C");
-      break;
-    case 13:
-      array.push("D");
-      break;
-    case 14:
-      array.push("E");
-      break;
-    case 15:
-      array.push("F");
-      break;
-    default:
-      array.push(val.toString());
-  }
+	switch (val) {
+		case 10:
+			array.push("A");
+			break;
+		case 11:
+			array.push("B");
+			break;
+		case 12:
+			array.push("C");
+			break;
+		case 13:
+			array.push("D");
+			break;
+		case 14:
+			array.push("E");
+			break;
+		case 15:
+			array.push("F");
+			break;
+		default:
+			array.push(val.toString());
+	}
 }
